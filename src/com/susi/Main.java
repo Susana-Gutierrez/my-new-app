b7847728ca4d60a598884a01ad2544cc3f26f418
Susana Gutierrez <susanagabriela_ap@hotmail.com>
2022-09-12 20:54:40 -0700

    match-class

--- src/com/susi/Main.java
+++ src/com/susi/Main.java
@@ -2,6 +2,7 @@ package com.susi;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
@@ -91,6 +92,93 @@ public class Main {
         System.out.println(10 - 2);
         System.out.println(10 * 2 + 7);
         System.out.println(10 % 3);
+
+        // Math
+
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.max(6, 10.5));
+        System.out.println(Math.min(6, 10.5));
+        System.out.println((int) Math.pow(5, 2));
+        System.out.println((int) Math.sqrt(25));
+        System.out.println(Math.PI);
+
+        // Comparison Operators
+
+        int khalidAge = 18;
+        int mariamAge = 20;
+        boolean isKhalidOlderThanMariam = khalidAge > mariamAge;
+        System.out.println(isKhalidOlderThanMariam);
+        System.out.println(mariamAge < khalidAge);
+        System.out.println(khalidAge == mariamAge);
+        System.out.println(khalidAge != mariamAge);
+
+        boolean isNewAdult = false;
+        boolean isStudent = true;
+        boolean isSusisFriend = true;
+        System.out.println(isNewAdult && isStudent);
+        System.out.println(isNewAdult || isStudent);
+        System.out.println((isNewAdult || isStudent) && isSusisFriend);
+
+        // If statements
+
+        int age = 17;
+
+        if (age >= 18) {
+            System.out.println("I'm an adult");
+        } else if (age >= 16 && age < 18) {
+            System.out.println("I'm almost an adult");
+        } else {
+            System.out.println("I'm not an adult");
+        }
+
+        String message = age >= 18 ? "Hooray... I am an adult" : "I am not an adult";
+        System.out.println(message);
+
+        // Switch Statement
+
+        String gender = "FEMALE";
+        if (gender.equals("FEMALE")) {
+            System.out.println("FEMALE");
+        } else if (gender.equals("MALE")) {
+            System.out.println("MALE");
+        } else if (gender.equals("PREFER_NOT_SAY")) {
+            System.out.println("PREFER NOT TO SAY");
+        } else {
+            System.out.println("NO VALUE");
+        }
+
+        switch (gender) {
+            case "FEMALE":
+                System.out.println("FEMALE");
+                break;
+            case "MALE":
+                System.out.println("MALE");
+                break;
+            case "PREFER_NOT_SAY":
+                System.out.println("PREFER NOT TO SAY");
+                break;
+            default:
+                System.out.println("NO VALUE");
+        }
+
+        //Arrays
+
+        int newZero = 0;
+        int newOne = 1;
+
+        int [] numbers = new int[3];
+        numbers[0] = 0;
+        numbers[1] = 1;
+        numbers[2] = 2;
+        System.out.println(Arrays.toString(numbers));
+
+        int [] newNumbers = {3, 4, 5};
+        System.out.println(Arrays.toString(newNumbers));
+        System.out.println(newNumbers.length);
+
+        String [] arrayNames = {"Ali", "Maria"};
+        System.out.println(Arrays.toString(arrayNames));
+        System.out.println(arrayNames.length);
 
     }
 
